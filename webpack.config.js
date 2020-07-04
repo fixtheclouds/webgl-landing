5331f435a45448121ee4a7c1f3c39533c63fd118
Egor Romanov <unsaidxpl@gmail.com>
2020-07-04 21:28:05 +0300

    Configure basic project

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+    publicPath: '/dist/'
+  },
+  mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin('styles.css'),
+    new webpack.ProvidePlugin({
+      _: 'lodash'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.sass$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.ttf$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /node_modules/,
+          name: 'vendor',
+          chunks: 'initial',
+          enforce: true
+        }
+      }
+    }
+  }
+};
